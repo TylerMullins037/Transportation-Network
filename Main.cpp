44556ad1ec0e755cb8ce74eba252fae812efc687
TylerMullins037 <144743073+TylerMullins037@users.noreply.github.com>
2023-10-12 17:48:32 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,29 +10,81 @@ int main() {
     map<string, City> cities;
 
     // Add cities and connections (you can populate this part)
-    addConnection(cities, "CityA", "CityB", 1, false); 
-    addConnection(cities, "CityB", "CityC", 2, false);   
-    addConnection(cities, "CityC", "CityD", 5, false);
-    addConnection(cities, "CityD", "CityZ", 1, false);   
-    addConnection(cities, "CityA", "CityK", 2, false); 
-    addConnection(cities, "CityK", "CityG", 1, false);   
-    addConnection(cities, "CityG", "CityZ", 10, false); 
-    addConnection(cities, "CityB", "CityE", 3, false);   
-    addConnection(cities, "CityC", "CityE",4, false);
-    addConnection(cities, "CityD", "CityE", 4, false);
-    addConnection(cities, "CityK", "CityE", 3, false);
-    addConnection(cities, "CityG", "CityE", 2, false);
-    addConnection(cities, "CityH", "CityE", 3, false);
-    addConnection(cities, "CityH", "CityD", 2, false);
-    addConnection(cities, "CityG", "CityH", 4, false);
-    addConnection(cities, "CityH", "CityZ", 3, false);
+    
+
+    addConnection(cities, "Astora", "Heide", 700, false); 
+    addConnection(cities, "Astora", "The Ringed City", 800, false);
+
+    addConnection(cities, "Astora", "Anor Londo", 400, false);
+    addConnection(cities, "Astora", "Thoroland", 300, false);
+    addConnection(cities, "Thoroland", "Carim", 200, false);
+
+    addConnection(cities, "Carim", "Vinheim", 200, false);
+    addConnection(cities, "Vinheim", "Undead Asylum", 300, false);
+    addConnection(cities, "Anor Londo", "Undead Asylum", 200, false);
+
+    addConnection(cities, "Anor Londo", "Oolacile", 700, false);
+
+
+
+
+    addConnection(cities, "Heide", "Unnamed Eastern Lands", 900, false);   
+    addConnection(cities, "Olaphis", "Unnamed Eastern Lands", 1000, false);
+    addConnection(cities, "Olaphis", "Heide", 1200, false);   
+  
+    addConnection(cities, "Unnamed Eastern Lands", "Far East", 1000, false); 
+
+    addConnection(cities, "Oolacile", "The Ringed City", 450, false);
+ 
+
+
+
+    addConnection(cities, "Vinheim", "Berenike", 700, false);
+
+     
+ 
+    addConnection(cities, "Berenike", "Balder", 150, false);
+    addConnection(cities, "Oolacile", "Balder", 650, false);
+
+    addConnection(cities, "Balder", "Undead Asylum", 650, false);
+    
+    addConnection(cities, "Five Finger Delta", "Catarina", 600, false);
+    addConnection(cities, "The Great Swamp", "Five Finger Delta", 700, false);
+    addConnection(cities, "Oolacile", "The Great Swamp", 700, false);
+     addConnection(cities, "Catarina", "Berenike", 200, false);
+   
+
+
+
+
+    //addConnection(cities, "A", "B", 3, false);
+    //addConnection(cities, "A", "D", 3, false);
+    //addConnection(cities, "A", "E", 4, false);
+
+    //addConnection(cities, "B", "D", 2, false);
+    //addConnection(cities, "B", "C", 4, false);
+
+    //addConnection(cities, "D", "G", 4, false);
+    //addConnection(cities, "D", "E", 1, false);
+    //addConnection(cities, "D", "F", 2, false);
+
+    //addConnection(cities, "E", "F", 2, false);
+
+    //addConnection(cities, "C", "G", 1, false);
+    //addConnection(cities, "G", "H", 6, false);
+    //addConnection(cities, "G", "F", 1, false);
+    //addConnection(cities, "F", "H", 5, false);
+    //addConnection(cities, "C", "H", 5, false);
+ 
 
     
 
     // Add more connections as needed
 
     // Example usage of shortestPath function
-    vector<string> path = shortestPath(cities, "CityA", "CityZ");
+    string start = "Carim";
+        string end = "Oolacile";
+    vector<string> path = shortestPath(cities, start, end);
 
     // Output the shortest path
     cout << "Shortest Path: ";
@@ -42,7 +94,7 @@ int main() {
     cout << "End" << endl;
 
     // Example usage of citiesWithinDistance function
-    vector<string> citiesWithinDist = citiesWithinDistance(cities, "CityA", 15);
+    vector<string> citiesWithinDist = citiesWithinDistance(cities, start, 15);
 
     // Output cities within a certain distance
     cout << "Cities within 15 units from StartCity: ";
@@ -62,3 +114,4 @@ int main() {
     }
     return 0;
 }
+
